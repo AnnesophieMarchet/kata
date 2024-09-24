9fea8e8a52912c834ed0c79c18ecbb3a6ff5f230
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-09-24 09:59:01 +0200

    fist kata

---
+++ recentMovies.js
@@ -0,0 +1,38 @@
+const recentMovies = [
+  {
+    title: "The Great Adventure",
+    releaseDate: "2023-07-15",
+    actors: ["Alex Johnson", "Samantha Bloom", "Michael Cane"],
+  },
+  {
+    title: "Lost in Time",
+    releaseDate: "2023-05-22",
+    actors: ["Clara Hughes", "Robert Smith", "Emily Stone"],
+  },
+  {
+    title: "Mystery of the Blue Ocean",
+    releaseDate: "2023-08-30",
+    actors: ["Henry Gold", "Natalie Port", "Bruce Wayne"],
+  },
+  {
+    title: "Cybernetic",
+    releaseDate: "2023-09-12",
+    actors: ["Aaron Stark", "Lisa Ray", "Jeremy Irons"],
+  },
+  {
+    title: "The Hidden World",
+    releaseDate: "2023-10-05",
+    actors: ["Sophia Loren", "Daniel Craig", "Emma Watson"],
+  },
+];
+
+function filterByTitle(recentMovies, searchedTitle) {
+  return recentMovies.filter((recentMovies) =>
+    recentMovies.title.toLowerCase().includes(searchedTitle.toLowerCase())
+  );
+}
+
+const res = filterByTitle(recentMovies, "coucou");
+const res1 = filterByTitle(recentMovies, "the");
+
+console.log(res1);
